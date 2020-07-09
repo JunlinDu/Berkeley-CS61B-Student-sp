627ced9d10d7a874a772b9de63cd52fce199a762
Junlin Du <matthewdu7@gmail.com>
2020-07-09 17:14:54 +1000

    ✅ HW1 Done

---
+++ hw1/synthesizer/GuitarString.java
@@ -0,0 +1,41 @@
+package synthesizer;
+
+public class GuitarString {
+    /** Constants. The keyword final means the values cannot be
+     * changed at runtime. */
+    private static final int SR = 44100;      // Sampling Rate
+    private static final double DECAY = .996; // energy decay factor
+
+    /* Buffer for storing sound data. */
+    private BoundedQueue<Double> buffer;
+
+    /* Create a guitar string of the given frequency.  */
+    public GuitarString(double frequency) {
+        this.buffer = new ArrayRingBuffer<>((int)Math.round(this.SR/frequency));
+    }
+
+
+    /* Pluck the guitar string by replacing the buffer with white noise. */
+    public void pluck() {
+        for (int i = 0; i < buffer.capacity(); i++) {
+            buffer.enqueue(Math.random() - 0.5);
+        }
+    }
+
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm. 
+     */
+    public void tic() {
+        Double dequeuedItem = buffer.dequeue();
+        Double frontItem;
+        buffer.enqueue(this.DECAY * 0.5 * (dequeuedItem + buffer.peek()));
+    }
+
+    /* Return the double at the front of the buffer. */
+    public double sample() {
+        if(!buffer.isEmpty()) {
+            return buffer.peek();
+        }
+        return 0;
+    }
+}
