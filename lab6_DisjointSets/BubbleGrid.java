ce387345452b727fbe72b2a2f2a2956ef30bb19d
Junlin Du <matthewdu7@gmail.com>
2020-07-15 13:01:13 +1000

    🚧 Lab6 Challenge on Hold

--- lab6_DisjointSets/BubbleGrid.java
+++ lab6_DisjointSets/BubbleGrid.java
@@ -1,19 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class BubbleGrid {
     private int[][] gird;
     private UnionFind disjointSet;
     private int columnSize;
-    /**
-     * [] [] [] [] [] []
-     * -- -- -- -- -- --
-     * -- -- -- -- -- --
-     * -- -- -- -- -- --
-     * -- -- -- -- -- -- */
-    public BubbleGrid() {
-        gird = new int[][]
-                {{1, 1, 1, 1}, {1, 0, 1, 1}, {0, 0, 0, 1}};
-        this.columnSize = gird.length;
+    private int rowSize;
+
+    public BubbleGrid(int[][] grid) {
+        this.gird = grid;
+        this.columnSize = grid.length;
+        this.rowSize = gird[grid.length-1].length;
+        this.disjointSet = new UnionFind(rowSize * columnSize - rowSize);
+        this.disjointSet = setDisjointSet(this.gird, this.disjointSet);
+    }
+
+    private UnionFind setDisjointSet(int[][] grid, UnionFind disjointSet) {
+        for(int row = 1; row < grid.length; row++) {
+            for(int col = 0; col < grid[0].length; col++) {
+                //
+            }
+        }
+        return null;
+    }
+
+    private void setLowerRows() {
+
     }
 
     public int[][] getGird() {
@@ -26,15 +39,23 @@ public class BubbleGrid {
 
     private static void print2DIntArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int x = 0; x < array[x].length; x++) {
-                System.out.print(array[x][i] + " ");
+            for(int x = 0; x < array[i].length; x++) {
+                System.out.print(array[i][x]);
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        BubbleGrid bubbleGrid = new BubbleGrid();
+        BubbleGrid bubbleGrid = new
+                BubbleGrid(new int[][]{{1, 1, 0}, {1, 0, 0}, {1, 1, 0}, {1, 1, 1}});
         print2DIntArray(bubbleGrid.getGird());
+
+        System.out.println();
+        System.out.println("Testing");
+        System.out.println(Arrays.toString(bubbleGrid.getGird()[1]));
+        System.out.println(bubbleGrid.getGird()[1][2]);
+        System.out.println("Row Size " + bubbleGrid.rowSize);
+        System.out.println("Column Size " + bubbleGrid.columnSize);
     }
 }
