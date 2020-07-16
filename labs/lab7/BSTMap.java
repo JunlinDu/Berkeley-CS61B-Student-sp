6e5a19e3adfae8c3fbeeb1ac6e35c2b292702134
Junlin Du <matthewdu7@gmail.com>
2020-07-17 03:26:19 +1000

    🚧 Lab7 Def Imp Done (rm,set,itr to be Imped later)

--- labs/lab7/BSTMap.java
+++ labs/lab7/BSTMap.java
@@ -1,13 +1,15 @@
 import edu.princeton.cs.algs4.BST;
+import edu.princeton.cs.algs4.In;
 
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K, V> implements Map61B<K, V> {
+import static org.junit.Assert.assertTrue;
 
-    private class Node<K extends Comparable<K>, V> {
-        Node left;
-        Node right;
+public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
+
+    private class Node {
+        Node left, right;
         K key;
         V value;
         Node (K key, V value) {
@@ -25,55 +27,86 @@ public class BSTMap<K, V> implements Map61B<K, V> {
 
     @Override
     public void clear() {
-
+        this.size = 0;
+        root = null;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return get(key) != null;
+    }
+
+    public Node getRecursive(K key, Node node) {
+        if (node == null || key.equals(node.key)) {
+            return node;
+        }else if (key.compareTo(node.key) < 0) {
+            return getRecursive(key, node.left);
+        } else {
+            return getRecursive(key, node.right);
+        }
     }
 
     @Override
-    public Object get(Object key) {
-        return null;
+    public V get(K key) {
+        Node node = getRecursive(key, this.root);
+        if (node == null) return null;
+        return node.value;
     }
+
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
+    }
+
+    public Node putRecursive(K key, V value, Node node)
+    {
+        if(node == null) return new Node(key, value);
+
+        if(key.compareTo(node.key) < 0) {
+            node.left = putRecursive(key, value, node.left);
+        } else {
+            node.right = putRecursive(key, value, node.right);
+        }
+        return node;
     }
 
     @Override
-    public void put(Object key, Object value) {
-
+    public void put(K key, V value) {
+        if (root == null) {
+            root = new Node(key, value);
+            this.size++;
+            return;
+        }
+        putRecursive(key, value, this.root);
+        this.size++;
     }
 
     @Override
     public Set keySet() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public Object remove(Object key) {
-        return null;
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public Object remove(Object key, Object value) {
-        return null;
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator iterator() {
-        return null;
-    }
-
-    public void printInOrder() {
-
+        throw new UnsupportedOperationException();
     }
 
     public static void main(String[] args) {
-
+        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        for (int i = 4; i < 10; i++) {
+            b.put("hi" + i, 1 + i);
+        }
     }
 
 }
