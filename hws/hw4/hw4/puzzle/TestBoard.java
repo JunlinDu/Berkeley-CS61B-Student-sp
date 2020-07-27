ae0485980271bd8405415749978dcf5a9bae5605
Junlin Du <matthewdu7@gmail.com>
2020-07-27 22:03:32 +1000

    hw4 start

--- hws/hw4/hw4/puzzle/TestBoard.java
+++ hws/hw4/hw4/puzzle/TestBoard.java
@@ -15,6 +15,7 @@ public class TestBoard {
                 cnt += 1;
             }
         }
+
         Board b = new Board(x);
         assertEquals("Your Board class is not being initialized with the right values.", 0, b.tileAt(0, 0));
         assertEquals("Your Board class is not being initialized with the right values.", 1, b.tileAt(0, 1));
